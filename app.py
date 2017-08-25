89395aefde43bc09677b3f13881c3ced38e2a846
Philip Kwesiga <phil.kwesiga@gmail.com>
2017-08-25 06:13:29 +0300

    Created user authentication

--- app.py
+++ app.py
@@ -30,6 +30,100 @@ class Users(db.Model):
     def __repr__(self):
         return '<Users %r>' % self.username
 
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+        #self.password_hash = pass_hash.decode("utf-8", "ignore")'
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({'id':self.id})
+
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None
+        except BadSignature:
+            return None
+        user = Users.query.get(data['id'])
+        return user
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    user = Users.verify_auth_token(username_or_token)
+    if not user:
+        user = Users.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)
+    if Users.query.filter_by(username=username).first() is not None:
+        abort(400)
+    user = Users(username, password)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username':user.username}), 201,
+            {'Location':url_for('get_user', id=user.id, _extername=True)})
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = Users.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username':user.username})
+
+
+@app.route('/api/token', methods=['GET'])
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token':token.decode('ascii'), 'duration':600})
+
+
+@app.route('/api/users/')
+@auth.login_required
+def get_resource():
+    return jsonify({'data':'Hello, %s!' % g.user.username})
+
+
+@app.route('/')
+def index():
+    return "<h1 style='color:red'>Hello there</h1>"
+
+"""
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    user = Users(request.form['username'], request.form['name'],
+                request.form['password'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token':token.decode('ascii')})
+"""
+
 
 if __name__ == "__main__":
     app.run()
